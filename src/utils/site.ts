f0fe66c5f0b73b2f202cc032da0bc6a42cf075f1
yogdiboswe <yogdiboswe@gmail.com>
2025-07-15 15:51:05 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Gilir Dimobil';
-export const desk = 'Video Bokep Indo Jepang Abg Hijab Lokal Tante Viral Terbaru Bocil Ngentot Jilbab Smp Colmek';
-export const url = 'https://bokepgilirdimobil.pages.dev';
-export const terbit = '2025-05-16T00:01:03Z';
